d8356174953325b2d69751d481479b8fdfc01301
ranagoharayoub <ranagohar1163@gmail.com>
2021-04-21 02:29:13 +0500

    change

--- src/Pages/VaccineNews.js
+++ src/Pages/VaccineNews.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import './VaccineNews.css'
 
 function VaccineNews() {
     return (
-        <div>
-             <h1>VaccineNews</h1>
+        <div className='news-cont'>
+             <div className='news-sect'>
+                 <div className='news-slice'>
+                     <div className='first-img'><img alt='img'></img></div>
+                     <div className='headline'><h3>News-Bullet</h3></div>
+                     <div className='second-img'><img alt='img'></img></div>
+                 </div>
+                 <div className='btn'>
+                    <button className='recent-btn'>RECENT NEWS</button>
+                    <button className='older-btn'>OLDER NEWS</button>
+                 </div>
+             </div>
         </div>
     )
 }
--- src/Pages/VaccinePortal.js
+++ src/Pages/VaccinePortal.js
@@ -4,7 +4,11 @@ import './VaccinePortal.css'
 function VaccinePortal() {
     return (
         <div className='portal-cont'>
-             
+             <div className='cards'>
+                <p className='company-name'>BIONTECH/ PFIZER</p>
+                <p className='cat'>Category: <p style={{color:'#F88B5A', marginLeft:'10px'}}>RNA Based Vaccine</p></p>
+                <p className='discription'>Description: <p style={{color:'#F88B5A', marginLeft:'5px'}}>3 LNP-mRNAs; BNT162</p></p>
+             </div>
         </div>
     )
 }
